53b89cf12bee03d8830d4e24f8736f26e3ed2253
Huei <huei90@gmail.com>
2014-04-28 09:49:10 +0800

    init
    1. Move `/test/` folder to root folder
    2. Move `/lib/` folder to root folder

---
+++ README.md
@@ -0,0 +1,35 @@
+# Node Generator [![Build Status](https://secure.travis-ci.org/easy-node/generator-easy-node.svg?branch=master)](https://travis-ci.org/easy-node/generator-easy-node)
+
+> Create a node.js module with [Yeoman][], including nodeunit unit tests.
+
+This generator is based of
+[generator-node](https://github.com/yeoman/generator-node/), authored by the
+yeoman team.
+
+This is original from [generator-node](https://github.com/yeoman/generator-node/), but easy-node modify the following features.
+
+1. Move `/test/` folder to root folder.
+2. Move `/lib/` folder to root folder.
+
+[Yeoman]: http://yeoman.io/
+
+
+## Installation
+
+Install the generator by running: `npm install -g generator-easy-node`.
+
+
+## Usage
+
+At the command-line, cd into an empty directory, run this command and follow the prompts.
+
+```
+yo node
+```
+
+_Note that this template will generate files in the current directory, so be sure to change to a new directory first if you don't want to overwrite existing files._
+
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
---
+++ app/index.js
@@ -0,0 +1,113 @@
+'use strict';
+
+var chalk = require('chalk');
+var util = require('util');
+var path = require('path');
+var npmName = require('npm-name');
+var yeoman = require('yeoman-generator');
+
+var NodeGenerator = module.exports = function NodeGenerator(args, options) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({
+      bower: false,
+      skipInstall: options['skip-install']
+    });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+util.inherits(NodeGenerator, yeoman.generators.NamedBase);
+
+NodeGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+  var log = this.log;
+
+  console.log(
+    this.yeoman +
+    '\nThe name of your project shouldn\'t contain "node" or "js" and' +
+    '\nshould be a unique ID not already in use at search.npmjs.org.');
+
+  var prompts = [{
+    name: 'name',
+    message: 'Module Name',
+    default: path.basename(process.cwd()),
+    filter: function (input) {
+      var done = this.async();
+
+      npmName(input, function (err, available) {
+        if (!available) {
+          log.info(chalk.yellow(input) + ' already exists on npm. You might want to use another name.');
+        }
+
+        done(input);
+      });
+    }
+  }, {
+    name: 'description',
+    message: 'Description',
+    default: 'The best module ever.'
+  }, {
+    name: 'homepage',
+    message: 'Homepage'
+  }, {
+    name: 'license',
+    message: 'License',
+    default: 'MIT'
+  }, {
+    name: 'githubUsername',
+    message: 'GitHub username'
+  }, {
+    name: 'authorName',
+    message: 'Author\'s Name'
+  }, {
+    name: 'authorEmail',
+    message: 'Author\'s Email'
+  }, {
+    name: 'authorUrl',
+    message: 'Author\'s Homepage'
+  }];
+
+  this.currentYear = (new Date()).getFullYear();
+
+  this.prompt(prompts, function (props) {
+    this.slugname = this._.slugify(props.name);
+    this.safeSlugname = this.slugname.replace(
+      /-+([a-zA-Z0-9])/g,
+      function (g) { return g[1].toUpperCase(); }
+    );
+
+    if(props.githubUsername){
+      this.repoUrl = 'https://github.com/' + props.githubUsername + '/' + this.slugname;
+    } else {
+      this.repoUrl = 'user/repo';
+    }
+
+    if (!props.homepage) {
+      props.homepage = this.repoUrl;
+    }
+
+    this.props = props;
+
+    cb();
+  }.bind(this));
+};
+
+NodeGenerator.prototype.lib = function lib() {
+  this.template('name.js', this.slugname + '.js');
+};
+
+NodeGenerator.prototype.test = function test() {
+  this.template('name_test.js', this.slugname + '_test.js');
+};
+
+NodeGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('jshintrc', '.jshintrc');
+  this.copy('gitignore', '.gitignore');
+  this.copy('travis.yml', '.travis.yml');
+
+  this.template('README.md');
+  this.template('Gruntfile.js');
+  this.template('_package.json', 'package.json');
+};
