4b3c16b79dfc7ce7db6b8fae9ddac8e1d84c9457
Huei Tan <huei90@gmail.com>
2014-04-30 22:39:54 +0800

    Update to the extend inheritance method https://github.com/yeoman/generator-node/pull/36

--- app/index.js
+++ app/index.js
@@ -1,115 +1,110 @@
 'use strict';
 
 var chalk = require('chalk');
-var util = require('util');
 var path = require('path');
 var npmName = require('npm-name');
 var yeoman = require('yeoman-generator');
 
-var NodeGenerator = module.exports = function NodeGenerator(args, options) {
-  yeoman.generators.Base.apply(this, arguments);
+var NodeGenerator = module.exports = yeoman.generators.Base.extend({
+    initializing: function () {
+        this.pkg = require('../package.json');
+    },
 
-  this.on('end', function () {
-    this.installDependencies({
-      bower: false,
-      skipInstall: options['skip-install']
-    });
-  });
+    prompting: function () {
+        var cb = this.async();
 
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
-};
-util.inherits(NodeGenerator, yeoman.generators.NamedBase);
+        this.log(
+            this.yeoman +
+                '\nThe name of your project shouldn\'t contain "node" or "js" and' +
+                '\nshould be a unique ID not already in use at search.npmjs.org.');
 
-NodeGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
-  var log = this.log;
+        var prompts = [{
+            name: 'name',
+            message: 'Module Name',
+            default: path.basename(process.cwd()),
+            filter: function (input) {
+                var done = this.async();
 
-  console.log(
-    this.yeoman +
-    '\nThe name of your project shouldn\'t contain "node" or "js" and' +
-    '\nshould be a unique ID not already in use at search.npmjs.org.');
+                npmName(input, function (err, available) {
+                    if (!available) {
+                        this.log.info(chalk.yellow(input) + ' already exists on npm. You might want to use another name.');
+                    }
 
-  var prompts = [{
-    name: 'name',
-    message: 'Module Name',
-    default: path.basename(process.cwd()),
-    filter: function (input) {
-      var done = this.async();
+                    done(input);
+                });
+            }
+        }, {
+            name: 'description',
+            message: 'Description',
+            default: 'The best module ever.'
+        }, {
+            name: 'homepage',
+            message: 'Homepage'
+        }, {
+            name: 'license',
+            message: 'License',
+            default: 'MIT'
+        }, {
+            name: 'githubUsername',
+            message: 'GitHub username'
+        }, {
+            name: 'authorName',
+            message: 'Author\'s Name'
+        }, {
+            name: 'authorEmail',
+            message: 'Author\'s Email'
+        }, {
+            name: 'authorUrl',
+            message: 'Author\'s Homepage'
+        }];
 
-      npmName(input, function (err, available) {
-        if (!available) {
-          log.info(chalk.yellow(input) + ' already exists on npm. You might want to use another name.');
-        }
+        this.currentYear = (new Date()).getFullYear();
 
-        done(input);
-      });
+        this.prompt(prompts, function (props) {
+            this.slugname = this._.slugify(props.name);
+            this.safeSlugname = this.slugname.replace(
+                /-+([a-zA-Z0-9])/g,
+                function (g) { return g[1].toUpperCase(); }
+            );
+
+            if(props.githubUsername){
+                this.repoUrl = 'https://github.com/' + props.githubUsername + '/' + this.slugname;
+            } else {
+                this.repoUrl = 'user/repo';
+            }
+
+            if (!props.homepage) {
+                props.homepage = this.repoUrl;
+            }
+
+            this.props = props;
+
+            cb();
+        }.bind(this));
+    },
+
+    configuring: function () {
+        this.config.save();
+        this.copy('jshintrc', '.jshintrc');
+        this.copy('gitignore', '.gitignore');
+        this.copy('travis.yml', '.travis.yml');
+
+        this.template('_README.md', 'README.md');
+        this.template('_Gruntfile.js', 'Gruntfile.js');
+        this.template('_package.json', 'package.json');
+        this.template('_bower.json', 'bower.json');
+        this.template('_component.json', 'component.json');
+    },
+
+    writing: function () {
+        this.template('name.js', this.slugname + '.js');
+        this.template('name_test.js', this.slugname + '_test.js');
+    },
+
+    install: function () {
+        this.installDependencies({
+            bower: false,
+            skipInstall: this.options['skip-install']
+        });
     }
-  }, {
-    name: 'description',
-    message: 'Description',
-    default: 'The best module ever.'
-  }, {
-    name: 'homepage',
-    message: 'Homepage'
-  }, {
-    name: 'license',
-    message: 'License',
-    default: 'MIT'
-  }, {
-    name: 'githubUsername',
-    message: 'GitHub username'
-  }, {
-    name: 'authorName',
-    message: 'Author\'s Name'
-  }, {
-    name: 'authorEmail',
-    message: 'Author\'s Email'
-  }, {
-    name: 'authorUrl',
-    message: 'Author\'s Homepage'
-  }];
-
-  this.currentYear = (new Date()).getFullYear();
-
-  this.prompt(prompts, function (props) {
-    this.slugname = this._.slugify(props.name);
-    this.safeSlugname = this.slugname.replace(
-      /-+([a-zA-Z0-9])/g,
-      function (g) { return g[1].toUpperCase(); }
-    );
-
-    if(props.githubUsername){
-      this.repoUrl = 'https://github.com/' + props.githubUsername + '/' + this.slugname;
-    } else {
-      this.repoUrl = 'user/repo';
-    }
-
-    if (!props.homepage) {
-      props.homepage = this.repoUrl;
-    }
-
-    this.props = props;
-
-    cb();
-  }.bind(this));
-};
-
-NodeGenerator.prototype.lib = function lib() {
-  this.template('name.js', this.slugname + '.js');
-};
-
-NodeGenerator.prototype.test = function test() {
-  this.template('name_test.js', this.slugname + '_test.js');
-};
-
-NodeGenerator.prototype.projectfiles = function projectfiles() {
-  this.copy('jshintrc', '.jshintrc');
-  this.copy('gitignore', '.gitignore');
-  this.copy('travis.yml', '.travis.yml');
-
-  this.template('README.md');
-  this.template('Gruntfile.js');
-  this.template('_package.json', 'package.json');
-  this.template('_bower.json', 'bower.json');
-  this.template('_component.json', 'component.json');
-};
+});
